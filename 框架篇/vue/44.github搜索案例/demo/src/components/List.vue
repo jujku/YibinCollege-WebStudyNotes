ffdcff66c6cca8fd2e683249805af7e2ccc464aa
jujku <94424188+jujku@users.noreply.github.com>
2024-06-10 18:18:24 +0800

    github搜索完善

--- 框架篇/vue/44.github搜索案例/demo/src/components/List.vue
+++ 框架篇/vue/44.github搜索案例/demo/src/components/List.vue
@@ -1,13 +1,20 @@
 <template>
   <div>
-    <ul>
-      <li v-for="user in users" :key="user.id">
+    <!-- //搜搜结果 -->
+    <ul v-show="info.users.length">
+      <li v-for="user in info.users" :key="user.id">
         <a :href="user.html_url">
           <img :src="user.avatar_url" alt="" />
         </a>
         <p>{{ user.login }}</p>
       </li>
     </ul>
+    <!-- 欢迎页面 -->
+    <h1 v-show="info.isFirst">欢迎使用!</h1>
+    <!-- 加载页面 -->
+    <h1 v-show="info.isLoading">加载中....</h1>
+    <!-- 请求失败 -->
+    <p>{{ info.errMsg }}</p>
   </div>
 </template>
 
@@ -16,13 +23,18 @@ export default {
   name: "List",
   data() {
     return {
-      users: [],
+      info: {
+        users: [],
+        isFirst: true,
+        errMsg: "",
+        isLoading: false,
+      },
     };
   },
   mounted() {
-    this.$bus.$on("getUsers", (users) => {
-      console.log("ListComponent get users", users);
-      this.users = users;
+    this.$bus.$on("getUsers", (dataObj) => {
+      console.log("ListComponent get users", dataObj.users);
+      this.info = { ...this.info, ...dataObj }; //es6语法合并字符串
     });
   },
 };
--- 框架篇/vue/44.github搜索案例/demo/src/components/Search.vue
+++ 框架篇/vue/44.github搜索案例/demo/src/components/Search.vue
@@ -16,14 +16,31 @@ export default {
   },
   methods: {
     getUsers() {
+      this.$bus.$emit("getUsers", {
+        isFirst: false,
+        isLoading: true,
+        errMsg: "",
+        users: [],
+      });
       axios({
         method: "get",
         url: `https://api.github.com/search/users?q=${this.keyWord}`,
       }).then(
         (response) => {
-          this.$bus.$emit("getUsers", response.data.items);
+          this.$bus.$emit("getUsers", {
+            isFirst: false,
+            isLoading: false,
+            errMsg: "",
+            users: response.data.items,
+          });
         },
         (error) => {
+          this.$bus.$emit("getUsers", {
+            isFirst: false,
+            isLoading: false,
+            errMsg: error.message,
+            users: [],
+          });
           console.log(error.message);
         }
       );
